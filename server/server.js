3cd9c66c67fe8a9ba714e7b3fc2fa9ddd5cce47f
Sherwin Mina <sherwinmina@yahoo.com>
2017-04-21 13:11:46 -0700

    added socket.IO

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
