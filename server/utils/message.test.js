af6853ca15db37cbc90c01a7545d65721fd4a160
Sherwin Mina <sherwinmina@yahoo.com>
2017-04-22 09:44:07 -0700

    Mocha test for Messages

---
+++ server/utils/message.test.js
@@ -0,0 +1,17 @@
+var expect = require('expect');
+
+var {generatemessage} = require('./message');
+
+describe('generateMessage', () => {
+  it('should generate correct message object', () => {
+    var from ='Jen';
+    var text = 'Some Message';
+    var message = generatemessage(from, text);
+
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, text})
+    // store res in variable
+    // assert from match
+    // asset text matchMediaassert createdAt
+  });
+});
