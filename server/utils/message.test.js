1330b171796531851ce24c12c904b2adf787b5cd
Sherwin Mina <sherwinmina@yahoo.com>
2017-04-23 12:12:12 -0700

    updated mocha test suite

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generatemessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -13,5 +13,19 @@ describe('generateMessage', () => {
     // store res in variable
     // assert from match
     // asset text matchMediaassert createdAt
+  });
+});
+
+describe (generateLocationMessage, () => {
+  it('should generate correct location object', () => {
+    var from = 'Deb';
+    var latitude = 15;
+    var longtitude = 19;
+    var url = 'https://www.google.com/maps/?=15,19';
+    var message = generateLocationMessage(from , latitude, longtitude);
+
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, url});
+
   });
 });
