24daf1ee78713604b8047331c1b60ebf95af61f0
Sherwin Mina <sherwinmina@yahoo.com>
2017-04-22 18:20:37 -0700

    added geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -28,3 +28,17 @@ jQuery('#message-form').on('submit', function (e) {
 
   });
 });
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function () {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser');
+  }
+
+  navigator.geolocation.getCurrentPosition(function (getCurrentPosition) {
+    console.log(position);
+  }, function () {
+    alert('Unable to fetch location');
+  });
+
+});
