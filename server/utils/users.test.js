23cc447fcb48c3e6279f02c3af4cf3d4a4a659a4
Sherwin Mina <sherwinmina@yahoo.com>
2017-04-24 16:45:07 -0700

    update test

--- server/utils/users.test.js
+++ server/utils/users.test.js
@@ -37,11 +37,19 @@ describe('Users', () => {
   });
 
   it('should remove a user', () => {
+    var userId = '1';
+    var user = users.removeUser(userId);
 
+    expect(user.id).toBe(userId);
+    expect(users.users.length).toBe(2);
   });
 
   it('should not remove a user', () => {
+    var userId = '99';
+    var user = users.removeUser(userId);
 
+    expect(user.id).toNotExist();
+    expect(users.users.length).toBe(3);
   });
 
   it('should find a user', () => {
