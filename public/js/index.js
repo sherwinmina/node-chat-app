cf13915b86282fb1b1e9ac8f15ccc052b89add6d
Sherwin Mina <sherwinmina@yahoo.com>
2017-04-24 13:20:25 -0700

    fixed scrolling

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,29 @@
- var socket = io();
+var socket = io();
 
-  socket.on('connect', function () {
-    console.log('Connected to server');
-  });
+function scrollToBottom () {
+  // Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  // Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
 
-  socket.on('disconnect', function () {
-    console.log('Disconnected from server');
-  
-  });
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
+socket.on('connect', function () {
+  console.log('Connected to server');
+});
+
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+
+});
 
 
 socket.on('newMessage', function (message) {
@@ -20,11 +36,7 @@ socket.on('newMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
-
-  // var li = jQuery('<li></li>');
-  // li.text(`${message.from} ${formattedTime}: ${message.text}`);
-
-  // jQuery('#messages').append(li);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -37,7 +49,8 @@ socket.on('newLocationMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
-  
+  scrollToBottom();
+
 });
 
 jQuery('#message-form').on('submit', function (e) {
