a53cd69e2e939590428e32c1062383c75dadf8d8
Sherwin Mina <sherwinmina@yahoo.com>
2017-04-24 17:26:31 -0700

    appended updated user list

--- server/server.js
+++ server/server.js
@@ -48,7 +48,8 @@ io.on('connection', (socket) => {
 
     if (user) {
       io.to(user.room).emit('updateUserList', users.getUserList(user.room));
-      io.to(user.room).emit('newMessage', `${user.name}, has left`);
+      io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
+
     }
   });
 });
